a2ec9491c3515be765f8e246c09ef6c40bf0e5da
u-1 <shimaichi5973@gamil.com>
2024-08-19 07:50:37 +0900

    Entityクラスの修正

--- src/main/java/com/u_1/copypet/Entity/Pet.java
+++ src/main/java/com/u_1/copypet/Entity/Pet.java
@@ -1,14 +1,12 @@
 package com.u_1.copypet.Entity;
 
 import com.u_1.copypet.Entity.Enum.Gender;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class Pet {
 
   private int userId;
-  @NotBlank(message = "ペット名を入力してください")
   private String petName;//ペットの名前
   private int ageInDays;//ペットの年齢
   private Gender petGender;//ペットの性別
